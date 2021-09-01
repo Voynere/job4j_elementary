ed76b2b6f400199af5ef3c9bcc6c853c9f3a862a
Eugene <voynere@yandex.ru>
2021-09-01 13:05:37 +1000

    4.2. Тернарное сравнение [#189]

--- src/ru/job4j/condition/Film.java
+++ src/ru/job4j/condition/Film.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Film {
     public static String permission(int age) {
-        String label = age >= 18 ? "Please, enjoy it." : "You can't watch it.";
-        return label;
+        return age >= 18 ? "Please, enjoy it." : "You can't watch it.";
     }
 
     public static void main(String[] args) {

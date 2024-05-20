375cfb3088ed442f152e8ef989538e4d9f4be04c
Voynere <voynere@yandex.ru>
2024-05-20 13:45:41 +1000

    Улучшил код с помощью ChatGPT

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,22 +2,35 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+    // Метод для расчета идеального веса по формуле
+    private static double calculateWeight(short height, int heightOffset) {
+        return (height - heightOffset) * 1.15;
     }
 
+    // Идеальный вес для мужчины
+    public static double manWeight(short height) {
+        return calculateWeight(height, 100);
+    }
+
+    // Идеальный вес для женщины
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return calculateWeight(height, 110);
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height);
-        System.out.println("Man 187 is " + man);
-        System.out.println("Woman 187 is " + woman);
+        short height = 184;
+
+        // Проверяем, что рост находится в разумных пределах
+        if (height < 100 || height > 250) {
+            System.out.println("Height should be between 100 and 250 cm.");
+            return;
+        }
+
+        double man = manWeight(height);
+        double woman = womanWeight(height);
+
+        System.out.println("Man 184 is " + man);
+        System.out.println("Woman 184 is " + woman);
     }
 
 }

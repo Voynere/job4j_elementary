8e686b712411d8a1472314f4cfb20e388edce592
Voynere <voynere@yandex.ru>
2024-05-20 13:52:44 +1000

    Комментарии JavaDoc

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,17 +2,30 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    // Метод для расчета идеального веса по формуле
+    /**
+     * Метод для расчета идеального веса по формуле
+     * @param height
+     * @param heightOffset
+     * @return
+     */
     private static double calculateWeight(short height, int heightOffset) {
         return (height - heightOffset) * 1.15;
     }
 
-    // Идеальный вес для мужчины
+    /**
+     * Идеальный вес для мужчины
+     * @param height
+     * @return
+     */
     public static double manWeight(short height) {
         return calculateWeight(height, 100);
     }
 
-    // Идеальный вес для женщины
+    /**
+     * Идеальный вес для женщины
+     * @param height
+     * @return
+     */
     public static double womanWeight(short height) {
         return calculateWeight(height, 110);
     }
@@ -20,7 +33,9 @@ public class Fit {
     public static void main(String[] args) {
         short height = 184;
 
-        // Проверяем, что рост находится в разумных пределах
+        /**
+         * Проверяем, что рост находится в разумных пределах
+         */
         if (height < 100 || height > 250) {
             System.out.println("Height should be between 100 and 250 cm.");
             return;
